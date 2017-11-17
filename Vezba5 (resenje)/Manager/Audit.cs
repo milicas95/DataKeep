71bda24d7da853588e1bd5057365ce758a0d05e5
IKatarinaI <katarinaivosevic1995@gmail.com>
2017-11-17 14:37:57 +0100

    Audit log fajl

---
+++ Vezba5 (resenje)/Manager/Audit.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Manager
+{
+    public class Audit : IDisposable
+    {
+        private static EventLog customLog = null;
+        const string SourceName = "Manager.Audit";
+        const string LogName = "DataKeep";          //naziv naseg Log fajla u Windows Event Log
+
+        static Audit()
+        {
+            try
+            {
+                //pravi se customLog handle
+                if (!EventLog.SourceExists(SourceName))
+                {
+                    EventLog.CreateEventSource(SourceName, LogName);
+                }
+
+                customLog = new EventLog(LogName, Environment.MachineName, SourceName);
+            }
+            catch (Exception e)
+            {
+                customLog = null;
+                Console.WriteLine("Error while trying to create log handle. Error = {0}", e.Message);
+            }
+        }
+
+        public static void AuthenticationSuccess(string userName)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.UserAuthenticationSuccess, userName), EventLogEntryType.SuccessAudit);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.UserAuthenticationSuccess));
+            }
+        }
+
+        public static void AuthorizationSuccess(string userName, string serviceName)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.UserAuthorizationSuccess,userName,serviceName),EventLogEntryType.SuccessAudit);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.UserAuthorizationSuccess));
+            }
+        }
+
+        public static void AuthorizationFailed(string userName, string serviceName, string reason)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.UserAuthorizationFailed,userName,serviceName,reason), EventLogEntryType.Error);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.UserAuthorizationFailed));
+            }
+        }
+
+        public static void CreateSuccess(string dbName)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.CreateDBSuccess, dbName), EventLogEntryType.SuccessAudit);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.CreateDBSuccess));
+            }
+        }
+
+        public static void CreateFailed(string dbName,string reason)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.CreateDBFailed, dbName, reason), EventLogEntryType.Error);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.CreateDBFailed));
+            }
+        }
+
+        public static void DeleteSuccess(string dbName)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.DeleteDBSuccess, dbName), EventLogEntryType.SuccessAudit);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.DeleteDBSuccess));
+            }
+        }
+
+        public static void DeleteFailed(string dbName,string reason)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.DeleteDBFailed, dbName, reason), EventLogEntryType.Error);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.DeleteDBFailed));
+            }
+        }
+
+        public static void AddSuccess()
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.AddEntitySuccess), EventLogEntryType.SuccessAudit);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.AddEntitySuccess));
+            }
+        }
+
+        public static void AddFailed(string reason)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.AddEntityFailed, reason), EventLogEntryType.Error);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.AddEntityFailed));
+            }
+        }
+
+        public static void UpdateSuccess()
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.UpdateEntitySuccess), EventLogEntryType.SuccessAudit);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.UpdateEntitySuccess));
+            }
+        }
+
+        public static void UpdateFailed(string reason)
+        {
+            if (customLog != null)
+            {
+                //poziva metodu AuditEvents da bi ispisao poruku u Log fajl
+                customLog.WriteEntry(string.Format(AuditEvents.UpdateEntityFailed, reason), EventLogEntryType.Error);
+            }
+            else
+            {
+                throw new ArgumentException(string.Format("Error while trying to write event with id {0} to event log", (int)AuditEventTypes.UpdateEntityFailed));
+            }
+        }
+
+        public void Dispose()
+        {
+            if (customLog != null)
+            {
+                customLog.Dispose();
+                customLog = null;
+            }
+        }
+    }
+}
---
+++ Vezba5 (resenje)/Manager/AuditEvents.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+
+namespace Manager
+{
+    public enum AuditEventTypes
+    {
+        UserAuthenticationSuccess = 0,
+        UserAuthorizationSuccess = 1,
+        UserAuthorizationFailed = 2,
+        CreateDBSuccess = 3,
+        CreateDBFailed = 4,
+        DeleteDBSuccess = 5,
+        DeleteDBFailed = 6,
+        AddEntitySuccess = 7,
+        AddEntityFailed = 8,
+        UpdateEntitySuccess = 9,
+        UpdateEntityFailed = 10
+    }
+
+    public class AuditEvents
+    {
+        private static ResourceManager resourceManager = null;
+            private static object resourceLock = new object();
+
+            private static ResourceManager ResourceMgr
+            {
+                get
+                {
+                    lock (resourceLock)
+                    {
+                        if (resourceManager == null)
+                        {
+                            resourceManager = new ResourceManager(typeof(AuditEventsFile).FullName, Assembly.GetExecutingAssembly());
+                        }
+
+                        return resourceManager;
+                    }
+                }
+            }
+
+            public static string UserAuthenticationSuccess
+            {
+                get
+                {
+                    return ResourceMgr.GetString(AuditEventTypes.UserAuthenticationSuccess.ToString());
+                }
+            }
+
+            public static string UserAuthorizationFailed
+            {
+                get
+                {
+                    return ResourceMgr.GetString(AuditEventTypes.UserAuthorizationFailed.ToString());
+                }
+            }
+
+            public static string UserAuthorizationSuccess
+            {
+                get
+                {
+                    return ResourceMgr.GetString(AuditEventTypes.UserAuthorizationSuccess.ToString());
+                }
+            }
+
+        public static string CreateDBSuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.CreateDBSuccess.ToString());
+            }
+        }
+
+        public static string CreateDBFailed
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.CreateDBFailed.ToString());
+            }
+        }
+
+        public static string DeleteDBSuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.DeleteDBSuccess.ToString());
+            }
+        }
+
+        public static string DeleteDBFailed
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.DeleteDBFailed.ToString());
+            }
+        }
+
+        public static string AddEntitySuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.AddEntitySuccess.ToString());
+            }
+        }
+
+        public static string AddEntityFailed
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.AddEntityFailed.ToString());
+            }
+        }
+
+        public static string UpdateEntitySuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.UpdateEntitySuccess.ToString());
+            }
+        }
+
+        public static string UpdateEntityFailed
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.UpdateEntityFailed.ToString());
+            }
+        }
+    }
+    }
