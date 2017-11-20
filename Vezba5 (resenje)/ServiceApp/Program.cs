e329dd009fa6b0c57105b5140b2fc889de0b5591
NikolaBarna <>
2017-11-20 14:16:29 +0100

    Revert "Revert "ponedeljak 1""
    This reverts commit 660d59df975ed51522b0e0fa08addb6ecfcdf206.

--- Vezba5 (resenje)/ServiceApp/Program.cs
+++ Vezba5 (resenje)/ServiceApp/Program.cs
@@ -31,32 +31,31 @@ namespace ServiceApp
                     found = true;
                     break;
                 }
-            }
 
+            }
             NetTcpBinding binding = new NetTcpBinding();
-			binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
 
 			string address = "net.tcp://localhost:9999/Receiver";
 			ServiceHost host = new ServiceHost(typeof(WCFService));
-			host.AddServiceEndpoint(typeof(IDatabaseManagement), binding, address);
+            host.AddServiceEndpoint(typeof(IDatabaseManagement), binding, address);
 
-			///PeerTrust - for development purposes only to temporarily disable the mechanism that checks the chain of trust for a certificate. 
-			///To do this, set the CertificateValidationMode property to PeerTrust (PeerOrChainTrust) - specifies that the certificate can be self-issued (peer trust) 
-			///To support that, the certificates created for the client and server in the personal certificates folder need to be copied in the Trusted people -> Certificates folder.
-			///host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.PeerTrust;
+            ///PeerTrust - for development purposes only to temporarily disable the mechanism that checks the chain of trust for a certificate. 
+            ///To do this, set the CertificateValidationMode property to PeerTrust (PeerOrChainTrust) - specifies that the certificate can be self-issued (peer trust) 
+            ///To support that, the certificates created for the client and server in the personal certificates folder need to be copied in the Trusted people -> Certificates folder.
+            ///host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.PeerTrust;
 
-			///Custom validation mode enables creation of a custom validator - CustomCertificateValidator
-			host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
-			host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
-
-			///If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
-			host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+            ///Custom validation mode enables creation of a custom validator - CustomCertificateValidator
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+            
             if (found)
             {
                 ///Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
-                host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN,groupName);
+                host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN, groupName);
                 /// host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromFile("WCFService.pfx");
-
+                
                 try
                 {
                     host.Open();
